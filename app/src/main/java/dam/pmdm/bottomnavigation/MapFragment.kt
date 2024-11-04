fa3fc19aad6f022e5bbfee1fcb317b183e9e88b9
Lindsay <lin.barrero.martinez@gmail.com>
2024-11-04 10:21:20 +0100

    Refactor tab name to use with google maps.

--- app/src/main/java/dam/pmdm/bottomnavigation/MapFragment.kt
+++ app/src/main/java/dam/pmdm/bottomnavigation/MapFragment.kt
@@ -5,23 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import dam.pmdm.bottomnavigation.databinding.FragmentNotificationBinding
+import dam.pmdm.bottomnavigation.databinding.FragmentMapBinding
 
 class NotificationsFragment : Fragment() {
 
-    private var _binding: FragmentNotificationBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentMapBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentNotificationBinding.inflate(inflater, container, false)
+        binding = FragmentMapBinding.inflate(inflater, container, false)
         return binding.root
     }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
-    }
 }
+
